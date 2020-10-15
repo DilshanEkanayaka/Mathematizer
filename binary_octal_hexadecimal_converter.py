2e10ad2c9ede242ca66fab17ff8fe02f5a049a4d
DilshanEkanayaka <41530832+DilshanEkanayaka@users.noreply.github.com>
2020-10-15 23:29:40 +0530

    Create binary_octal_hexadecimal_converter.py

---
+++ binary_octal_hexadecimal_converter.py
@@ -0,0 +1,9 @@
+#https://www.facebook.com/dinuki.prasadi/posts/1413643328840057
+#subscribed by dilshansampath
+
+dec = 344
+
+print("The decimal value of", dec, "is:")
+print(bin(dec), "in binary.")
+print(oct(dec), "in octal.")
+print(hex(dec), "in hexadecimal.")
